68243bd19402f242f74eea9e1aa4d6f8a13495ac
Rociobsr <107640834+Rociobsr@users.noreply.github.com>
2023-11-26 23:22:39 -0300

    map

--- src/app/crear-busqueda/map/map.component.ts
+++ src/app/crear-busqueda/map/map.component.ts
@@ -1,44 +1,59 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { GmapService } from 'src/app/services/gmap.service';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { GmapService } from './../../services/gmap.service';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent  implements OnInit {
-  @ViewChild('map', {static: true}) mapElementRef: ElementRef;
+export class MapComponent implements OnInit {
+  @ViewChild('map', { static: true }) mapElementRef: ElementRef;
   googleMaps: any;
-  source: any = { lat: -33.0336892, lng: -71.535759 };
-  dest: any = { lat: -33.0336892, lng: -71.535759 };
+  lat: number;
+  long: number;
+  positionData: any[]; // Declarar positionData para almacenar la información de la posición
 
   constructor(
     private maps: GmapService,
     private renderer: Renderer2
   ) { }
 
-  ngOnInit() {}
-
-  ngAfterViewInit(){
-    this.loadMap();
-    
+  ngOnInit() {
+    this.printCurrentPosition();
   }
 
-  async loadMap(){
-    try{
-      let googleMaps: any = await this.maps.loadGoogleMaps(); 
-      const mapEl = this.mapElementRef.nativeElement;
-      const map = new googleMaps.Map(mapEl, {
-        center: { lat: this.source.lat, lng: this.source.lng },
-        disableDefaultUI: true,
-        zoom: 10,
-      });
-      const source_position = new googleMaps.LatLng(this.source.lat, this.source.lng);
-      map.setCenter(source_position);
-      this.renderer.addClass(mapEl, 'visible');
-    } catch(e){
-      console.log(e)
+  async printCurrentPosition() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      const { latitude, longitude } = coordinates.coords;
+
+      this.lat = latitude;
+      this.long = longitude;
+
+      console.log('Latitude:', this.lat);
+      console.log('Longitude:', this.long);
+
+      this.loadMap();
+
+    } catch (error) {
+      console.error('Error al obtener la posición:', error);
     }
   }
 
-}
+  async loadMap() {
+    try {
+      let googleMaps: any = await this.maps.loadGoogleMaps();
+      const mapEl = this.mapElementRef.nativeElement;
+      const map = new googleMaps.Map(mapEl, {
+        center: { lat: this.lat, lng: this.long },
+        disableDefaultUI: true,
+        zoom: 10,
+      });
+      const source_position = new googleMaps.LatLng(this.lat, this.long);
+      map.setCenter(source_position);
+      this.renderer.addClass(mapEl, 'visible');
+    } catch (e) {
+      console.log(e);
+    }
+  }}
