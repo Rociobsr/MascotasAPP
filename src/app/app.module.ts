c4a50ed6bdb6be644817709ac2e5f192c9a322af
Rociobsr <107640834+Rociobsr@users.noreply.github.com>
2023-12-14 20:30:14 -0300

    comp3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy, RouterModule } from '@angular/router';
-
+import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-
+import { ComptresComponent } from './components/comptres/comptres.component'; 
 import { ServiceRestService } from './services/service-rest.service';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,RouterModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },ServiceRestService ],
+  declarations: [AppComponent, ComptresComponent], 
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ServiceRestService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+
--- src/app/components/comptres/comptres.component.ts
+++ src/app/components/comptres/comptres.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-comptres',
   templateUrl: './comptres.component.html',
-  styleUrls: ['./comptres.component.scss'],
+  styleUrls: ['./comptres.component.scss'], 
 })
-export class ComptresComponent  implements OnInit {
+export class ComptresComponent {
+  petName: string = '';
+  description: string = '';
+  capturedImage: string | undefined;
 
-  constructor() { }
+  onSubmit() {
+    // Lógica para enviar el formulario, puedes agregar un servicio aquí.
+    console.log('Formulario Enviado');
+  }
 
-  ngOnInit() {}
-
+  onImageSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.capturedImage = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
+
