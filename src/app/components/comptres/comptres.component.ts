2881df204d9571e8f8b601679129c6691b079041
Rociobsr <107640834+Rociobsr@users.noreply.github.com>
2023-12-14 23:42:50 -0300

    ultm

--- src/app/components/comptres/comptres.component.ts
+++ src/app/components/comptres/comptres.component.ts
@@ -1,19 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-comptres',
   templateUrl: './comptres.component.html',
-  styleUrls: ['./comptres.component.scss'], 
+  styleUrls: ['./comptres.component.scss'],
 })
-export class ComptresComponent {
+export class ComptresComponent implements OnInit {
   petName: string = '';
   description: string = '';
-  capturedImage: string | undefined;
+  capturedImage: string | ArrayBuffer | null = null;
+  submittedReports: any[] = [];
+
+  ngOnInit() {
+    // Recuperar informes almacenados en el almacenamiento local
+    const storedReports = localStorage.getItem('submittedReports');
+    if (storedReports) {
+      this.submittedReports = JSON.parse(storedReports);
+    }
+  }
 
   onSubmit() {
-    // Lógica para enviar el formulario, puedes agregar un servicio aquí.
-    console.log('Formulario Enviado');
+    // Lógica para enviar el formulario
+    const newReport = {
+      petName: this.petName,
+      description: this.description,
+      capturedImage: this.capturedImage,
+    };
+
+    this.submittedReports.push(newReport);
+
+    // Limpiar los campos después de enviar el formulario
+    this.petName = '';
+    this.description = '';
+    this.capturedImage = null; // Restablecer a null después de enviar la imagen
+
+    // Almacenar informes en el almacenamiento local
+    localStorage.setItem('submittedReports', JSON.stringify(this.submittedReports));
   }
 
   onImageSelected(event: any) {
@@ -21,10 +43,18 @@ export class ComptresComponent {
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
-        this.capturedImage = reader.result as string;
+        this.capturedImage = reader.result;
       };
       reader.readAsDataURL(file);
     }
   }
-}
 
+  eliminarReporte(report: any) {
+    // Lógica para eliminar un informe
+    const index = this.submittedReports.findIndex(r => r === report);
+    if (index !== -1) {
+      this.submittedReports.splice(index, 1);
+      localStorage.setItem('submittedReports', JSON.stringify(this.submittedReports));
+    }
+  }
+} 
